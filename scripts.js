1e1eea390a7ed900c4f48c3ccf4df4fe762288b8
jcisking <krisloeffler@gmail.com>
2022-10-02 00:33:30 -0700

    modified css, html, and js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const btnOpen = document.querySelector(".iconShareContainer")
 const btnClose = document.querySelector(".iconShareContainer2")
 
-btnOpen.addEventListener("click", (e) => document.querySelector(".card").classList.add("share"))
+btnOpen.addEventListener("click", (e) => document.querySelector(".card").classList.toggle("share"))
 
 btnClose.addEventListener("click", (e) => document.querySelector(".card").classList.remove("share"))
 
